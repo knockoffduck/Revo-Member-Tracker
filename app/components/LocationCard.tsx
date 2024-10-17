90664a9ea153f3345017f1186765b36f33a968e3
knockoffduck <ddarm4@gmail.com>
2024-10-17 23:07:19 +0800

    implemented api and overall basic features

--- app/components/LocationCard.tsx
+++ app/components/LocationCard.tsx
@@ -2,21 +2,21 @@ import { Card } from "@/components/ui/card";
 import React from "react";
 import { Progress } from "@/components/ui/progress";
 
-interface Gym {
-	GymName: string;
-	Size: number;
-	LiveMemberCount: number;
-	MemberAreaRatio: number;
-	Percentage: number;
-}
+type Gym = {
+	name: string;
+	size: number;
+	member_count: number;
+	member_ratio: number;
+	percentage: number;
+};
 
 export const LocationCard = ({ gym }: { gym: Gym }) => {
 	return (
 		<Card className="flex flex-col  items-center justify-between border-black h-52 p-6">
-			<h2 className="text-xl">{gym.GymName}</h2>
-			<h3 className="text-4xl">{gym.LiveMemberCount}</h3>
+			<h2 className="text-xl">{gym.name}</h2>
+			<h3 className="text-4xl">{gym.member_count}</h3>
 			<Progress
-				value={gym.Percentage}
+				value={gym.percentage}
 				max={50}
 				className="h-8 rounded-lg"
 			></Progress>
--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,24 @@
 import { LocationCard } from "./components/LocationCard";
 
-export default function Home() {
-	const gyms = [
-		{
-			GymName: "Balcatta",
-			Size: 1180,
-			LiveMemberCount: 3,
-			MemberAreaRatio: 393.3333333333333,
-			Percentage: 5,
-		},
-		{
-			GymName: "Banksia Grove",
-			Size: 1100,
-			LiveMemberCount: 1,
-			MemberAreaRatio: 1100,
-			Percentage: 0,
-		},
-		{
-			GymName: "Belmont",
-			Size: 1050,
-			LiveMemberCount: 5,
-			MemberAreaRatio: 210,
-			Percentage: 0,
-		},
-		{
-			GymName: "Canning Vale",
-			Size: 2000,
-			LiveMemberCount: 7,
-			MemberAreaRatio: 285.7142857142857,
-			Percentage: 0,
-		},
-	];
+export default async function Home() {
+	type Gym = {
+		name: string;
+		size: number;
+		member_count: number;
+		member_ratio: number;
+		percentage: number;
+	};
+
+	let response = await fetch(
+		"https://revotracker.daffydvck.live/api/gyms/stats/latest"
+	);
+
+	let result = await response.json();
+
+	let gyms: Gym[] = result.data.data;
 
 	return (
-		<div className="grid w-screen h-screen px-8 justify-center pt-6 ">
+		<div className="grid w-screen h-screen px-8 justify-center pt-6 gap-6">
 			<h1 className="text-3xl font-bold">Revo Member Tracker</h1>
 			{gyms.map((gym, index) => (
 				<LocationCard key={index} gym={gym}></LocationCard>
