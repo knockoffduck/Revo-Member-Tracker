daad16370a6131d442646180e3716d51b04604f1
knockoffduck <ddarm4@gmail.com>
2024-10-18 00:32:48 +0800

    implemented colour changing bars

--- app/page.tsx
+++ app/page.tsx
@@ -10,16 +10,39 @@ export default async function Home() {
 	};
 
 	const response = await fetch(
-		"https://revotracker.daffydvck.live/api/gyms/stats/latest"
+		"https://revotracker.daffydvck.live/api/gyms/stats/latest",
+		{
+			method: "GET",
+			cache: "no-store", // Disable cache
+		}
 	);
 
 	const result = await response.json();
 
 	const gyms: Gym[] = result.data.data;
 
+	const convertToPerthTime = (isoString: string): string => {
+		const date = new Date(isoString);
+
+		// Format the date to Perth time (UTC+8)
+		const perthTimeFormatter = new Intl.DateTimeFormat("en-AU", {
+			timeZone: "Australia/Perth", // Convert to Perth timezone
+			month: "2-digit",
+			day: "2-digit",
+			hour: "2-digit",
+			minute: "2-digit",
+			hour12: false, // 24-hour format
+		});
+
+		return perthTimeFormatter.format(date);
+	};
+
+	const latestTime = convertToPerthTime(result.data.timestamp);
+
 	return (
 		<div className="grid w-screen h-screen px-8 justify-center pt-6 gap-6">
 			<h1 className="text-3xl font-bold">Revo Member Tracker</h1>
+			<h4 className="text-xl font-normal text-center">{latestTime}</h4>
 			{gyms.map((gym, index) => (
 				<LocationCard key={index} gym={gym}></LocationCard>
 			))}
--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -7,8 +7,10 @@ import { cn } from "@/lib/utils";
 
 const Progress = React.forwardRef<
 	React.ElementRef<typeof ProgressPrimitive.Root>,
-	React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(({ className, value, max, ...props }, ref) => (
+	React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> & {
+		color?: string;
+	}
+>(({ className, value, max, color, ...props }, ref) => (
 	<ProgressPrimitive.Root
 		ref={ref}
 		className={cn(
@@ -18,7 +20,7 @@ const Progress = React.forwardRef<
 		{...props}
 	>
 		<ProgressPrimitive.Indicator
-			className="h-full w-full flex-1 bg-primary transition-all"
+			className={cn("h-full w-full flex-1 bg-red-600 transition-all", color)}
 			style={{
 				transform: `translateX(-${100 - ((value || 0) / (max || 100)) * 100}%)`,
 			}}
